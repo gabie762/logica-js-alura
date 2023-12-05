0012ea151904fdfe51faf212c62c636fbb1d719a
gabie762 <gabihenriques821@gmail.com>
2023-12-04 21:12:22 -0300

    adicionada variavel tentativas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 alert('Boas vindas ao jogo do número secreto'); //manda uma mensagem para o início da página
 let mensagemDeVitoria = 'Parabains :)';
 let numeroSecreto = 15; //variável com o numero escolhido
+let tentativas = 1;
 console.log(numeroSecreto) //console.log é um comando que ajuda o desenvolvedor no código
 
 let chute;
@@ -12,9 +13,9 @@ console.log('Resultado da comparação:', chute == numeroSecreto);
 //Enquanto - loop de condição
 while(chute != numeroSecreto){
     chute = prompt('Escolha um número entre 1 e 30');  //uma mensagem com caixa de input do usuário
-
     if (chute == numeroSecreto){
-        alert('Você descobriu o número secreto: '+ numeroSecreto + '! :)');
+        alert('Você descobriu o número secreto: '+ numeroSecreto + '! :)\n'
+                + 'Foram ' + tentativas + ' tentativas');
     } else {
         if(numeroSecreto > chute){
             alert("O número secreto é maior que " + chute);
@@ -22,5 +23,6 @@ while(chute != numeroSecreto){
         else{
             alert("O número secreto é menor que " + chute);
         }
+        tentativas ++;
     }
 }
