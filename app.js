2fe89a19db3f3d77fba60a1b5418bd57a219ada5
gabie762 <gabihenriques821@gmail.com>
2023-12-29 20:23:08 -0300

    numero randomico atualizado

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 alert('Boas vindas ao jogo do número secreto'); //manda uma mensagem para o início da página
 let mensagemDeVitoria = 'Parabains :)';
-let numeroSecreto = 15; //variável com o numero escolhido
+let numeroSecreto = parseInt(Math.random() * 100 + 1); //variável com o numero escolhido
 let tentativas = 1;
 console.log(numeroSecreto) //console.log é um comando que ajuda o desenvolvedor no código
 
@@ -12,10 +12,9 @@ console.log('Resultado da comparação:', chute == numeroSecreto);
 
 //Enquanto - loop de condição
 while(chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 e 30');  //uma mensagem com caixa de input do usuário
+    chute = prompt('Escolha um número entre 1 e 100');  //uma mensagem com caixa de input do usuário
     if (chute == numeroSecreto){
-        alert('Você descobriu o número secreto: '+ numeroSecreto + '! :)\n'
-                + 'Foram ' + tentativas + ' tentativas');
+        break;
     } else {
         if(numeroSecreto > chute){
             alert("O número secreto é maior que " + chute);
@@ -26,3 +25,12 @@ while(chute != numeroSecreto){
         tentativas ++;
     }
 }
+
+let palavraTentativa = tentativas > 1 ? ' tentativas' : ' tentativa';
+alert('Você descobriu o número secreto: '+ numeroSecreto + '! :)\n' + 'Foram ' + tentativas + palavraTentativa);
+
+// if(tentativas > 1){
+//     alert('Você descobriu o número secreto: '+ numeroSecreto + '! :)\n' + 'Foram ' + tentativas + ' tentativas');
+// } else{
+//     alert('Você descobriu o número secreto: '+ numeroSecreto + '! :)\n' + 'Foram ' + tentativas + ' tentativa');
+// }
